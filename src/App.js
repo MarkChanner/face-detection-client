3f0147782f0f977bc946debfcd636ca4aa53a6b9
Mark Channer <markchanner@gmail.com>
2019-12-18 09:55:54 +0000

    Add error handling to fetch request

--- src/App.js
+++ src/App.js
@@ -88,7 +88,8 @@ class App extends React.Component {
             .then(response => response.json())
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(console.log);
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
